3c5f2e3fb643b9e26b14048f2bf966f78ee03b8a
miftahsara390-coder <miftahsara390@gmail.com>
2026-08-04 11:12:29 +0200

    fix line endings

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const logger = require('./middlewares/logger.middleware');
 const errorHandler = require('./middlewares/error.middleware');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
